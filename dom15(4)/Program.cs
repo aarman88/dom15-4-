29a33167bc17b13916e4a9d90baee2ed674b2c2d
Арман <@ARMAN>
2023-12-10 23:24:44 +0600

    Добавьте файлы проекта.

---
+++ dom15(4)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        // Получаем тип класса List<T>
+        Type listType = typeof(List<>);
+
+        // Получаем массив конструкторов
+        ConstructorInfo[] constructors = listType.GetConstructors();
+
+        // Выводим на экран список конструкторов
+        Console.WriteLine("Конструкторы класса List<T>:");
+        foreach (ConstructorInfo constructor in constructors)
+        {
+            Console.WriteLine(constructor);
+        }
+    }
+}
